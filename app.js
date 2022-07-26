93e919747caba8ae6e07a08d393ac8357c2ade83
baileyrec <kocakrecep68@gmail.com>
2022-07-26 16:54:15 +0200

    last

--- app.js
+++ app.js
@@ -3,25 +3,25 @@ const input = document.querySelector(".container input");
 const span = document.querySelector(".msg");
 const cityList = document.querySelector(".ajax-section .cities");
 
-// localStorage.setItem("apiKey", "ba4d3ccaf26615b67fae69295cf12553");
+// localStorage.setItem("apiKey", "5367e2ce8b28efe3b520e745cd510df2");
 
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    getWeatherDataFromApi();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeatherDataFromApi();
 });
 
-const getWeatherDataFromApi = async() =>{
-    let apiKey = localStorage.getItem("apiKey");
-    // alert(apiKey);
-    let inputVal = input.value;
-    let unitType = "metric";
-    let lang = "tr";
+const getWeatherDataFromApi = async () => {
+  let apiKey = localStorage.getItem("apiKey");
+  // alert(apiKey);
+  let inputVal = input.value;
+  let unitType = "metric";
+  let lang = "tr";
 
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=${unitType}&lang=${lang}`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=${unitType}&lang=${lang}`;
 
-    // const response = await fetch(url).then(response =>response.json());
-    try {
-        const response = await axios(url);
+  // const response = await fetch(url).then(response =>response.json());
+  try {
+    const response = await axios(url);
 
     //obj destructuring
     const { main, name, sys, weather } = response.data;
@@ -40,16 +40,18 @@ const getWeatherDataFromApi = async() =>{
     console.log("cityListCardsArray", cityListCardsArray);
 
     //cityName control
-    if(cityListCardsArray.length > 0){
-        const filteredArray = cityListCardsArray.filter(cityCard => cityCard.querySelector("span").innerText == name);
-        if(filteredArray.length > 0){
-            span.innerText = `You already know the weather for ${name}, Please search for another city 😉`;
-            setTimeout(()=>{
-                span.innerText = "";
-            }, 5000);
-            form.reset();
-            return;
-        }
+    if (cityListCardsArray.length > 0) {
+      const filteredArray = cityListCardsArray.filter(
+        (cityCard) => cityCard.querySelector("span").innerText == name
+      );
+      if (filteredArray.length > 0) {
+        span.innerText = `You already know the weather for ${name}, Please search for another city 😉`;
+        setTimeout(() => {
+          span.innerText = "";
+        }, 5000);
+        form.reset();
+        return;
+      }
     }
     // else{}
     const createdLi = document.createElement("li");
@@ -67,13 +69,11 @@ const getWeatherDataFromApi = async() =>{
     `;
     //append vs.prepend
     cityList.prepend(createdLi);
-    } 
-    catch (error) {
-        span.innerText = "City not found!";
-        setTimeout(()=>{
-            span.innerText = "";
-        }, 5000);
-    }    
-    form.reset();
-
-}
+  } catch (error) {
+    span.innerText = "City not found!";
+    setTimeout(() => {
+      span.innerText = "";
+    }, 5000);
+  }
+  form.reset();
+};
